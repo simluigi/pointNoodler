0e35da53dc2912f5f621e109a425bab309728c63
Sim Luigi <shimu.haltokyo@gmail.com>
2020-10-21 17:30:48 +0900

    List segregation/cylinder segregation complete.

--- pointNoodler/__init__.py
+++ pointNoodler/__init__.py
@@ -1,7 +1,7 @@
 """
 -- pointNoodler plugin --
 Author          : Sim Luigi
-Last xOffsetified   : 2020.10.19
+Last xOffsetified   : 2020.10.21
 
 End goal is to make a plugin that generates a 'noodle' (cylinder) that traverses any number of given points.
    (noodles sounds more fun than polyCylinder, right?) 
@@ -14,13 +14,10 @@ Benchmarks:
 -Extract selected edges (kMeshEdgeComponent)                (complete as of 2020.10.15)
 -Store selected edge/s' data (MFnSingleIndexedComponent)    (complete as of 2020.10.15) 
 -Add radius of 'noodle' as argument to be passed            (complete as of 2020.10.16)
-
 -Extract points from edge data                              (complete as of 2020.10.19)     
-    * getPointListFromEdges(), without using MItMeshEdge
-    * revert pointList argument to list type (previously converted to MPointArray)
-
--Implement base pointNoodler()                              (incomplete as of 2020.10.19)
-    * separation of pointList into separate noodles if the edges are not conected
+-Implement base pointNoodler()                              (complete as of 2020.10.21)
+    * separation of pointList into separate noodles if the edges are not conected - getPointListFromEdges()
+    (now returns a list of lists of MPoints, one list per noodle)
     
 -Get the upVecList                                          (incomplete as of 2020.10.19)
     * separate function (
@@ -32,12 +29,13 @@ Benchmarks:
 
 import maya.cmds as cmds
 import maya.OpenMaya as om
+import pprint
 
 # removing all other arguments until I can get it right and then understand the meaning & implementation of the original arguments (upVecList, parent)
 def pointNoodler(pointList, radius, upVecList=None):    
 
     # error handling: check if there are enough points to make at least 1 section
-    if len(pointList) -1 < 2:
+    if len(pointList)  < 2:
         raise Exception ("Need at least 2 points to perform pointNoodler!")
     
     numSections = len(pointList) -1        # -1 : minus the cap
@@ -146,7 +144,7 @@ def getMObject(node, obj):
         print ("Unable to get MObject. Have you specified an appropriate node?")
         raise
 
-# function for getting pointList from selected edges
+# function for getting pointList/s from selected edges, segregated into their corresponding segments if edges are not connected
 def getPointListFromEdges():
     edgeSelection = om.MSelectionList()
     edgeObject = om.MObject()
@@ -169,8 +167,9 @@ def getPointListFromEdges():
         edgeCount = edgeSIComponent.elementCount()        
         print ("Number of edges selected: ") + str(edgeCount)
 
-    pointList = []    
-    edgeVertexList = []                            
+    entryList = []                  # list of lists with format [edgeIndex, edgeP0Index, edgeP1Index]
+    segregatedList = []        # entries divided into respective connected pointLists
+
     edgeIndices = om.MIntArray() 
     edgeSIComponent.getElements(edgeIndices)       # returns all indices of selected edges
     
@@ -178,26 +177,47 @@ def getPointListFromEdges():
     edgeMesh = om.MFnMesh(edgeDagPath) 
     edgeUtil = om.MScriptUtil()
     edgeVertices = edgeUtil.asInt2Ptr()
+
+    # first, get all edgeIndices paired with their corresponding points and place in a entryList
+    # then add each list as an entry into inputList
     for index in xrange(edgeCount):
         edgeMesh.getEdgeVertices(edgeIndices[index], edgeVertices)
-        p0 = edgeUtil.getInt2ArrayItem(edgeVertices, 0, 0)
-        p1 = edgeUtil.getInt2ArrayItem(edgeVertices, 0, 1)
+        p0 = edgeUtil.getInt2ArrayItem(edgeVertices, 0, 0)      # p0 index
+        p1 = edgeUtil.getInt2ArrayItem(edgeVertices, 0, 1)      # p1 index
+        # inputList = [edgeIndices[index], p0, p1]              # [edgeIndex, p0Index, p1Index]
+        inputList = [p0, p1]                                    # [p0Index, p1Index]
+        entryList.append(inputList)                             # adds current list to entryList
+    
+    for entry in xrange(edgeCount):           
+        if entry == 0:
+            segregatedList.append(entryList[entry])         # automatically add first entry to segregated list            
+        else:
+            isFound = False            
+            for i in xrange(len(segregatedList)):                           # test for edge connections (exactly one common vertex)                                               
+                if entryList[entry][0] in segregatedList[i]:                # check if p0 is in in one of the lists (meaning edges are connected on incoming p0)
+                    segregatedList[i].append(entryList[entry][1])           # if so, add its partner (p1) to that list
+                    isFound = True                    
+                    break
+                if entryList[entry][1] in segregatedList[i]:                # check if p1 is in one of the lists (meaning edges are connected on incoming p1)
+                    segregatedList[i].append(entryList[entry][0])           # if so, add its partner (p0) to that list
+                    isFound = True
+                    break                                                                                 
+            if isFound == False:                                            # if not connected to any existing points, create a new entry in masterList designating a new cylinder to be created
+                segregatedList.append(entryList[entry])     
 
-        # filter algorithm here (do not append duplicates)
-        if not p0 in edgeVertexList:    
-            edgeVertexList.append(p0)
-        if not p1 in edgeVertexList:
-            edgeVertexList.append(p1)
-
-    edgeVertexList.sort()   # sort indices in ascending order
-
-    for index in xrange(len(edgeVertexList)):
-        point = om.MPoint()
-        edgeMesh.getPoint(edgeVertexList[index], point)  
-        pointList.append(point)
-        print "point # " + str(edgeVertexList[index]) + " appended to pointList with coordinates X: " + str(point[0]) + " Y: " + str(point[1]) + " Z: " + str(point[2])
-
-    return pointList
+    # final conversion of points from their indices 
+    masterList = []
+    for index in xrange(len(segregatedList)):        
+        pointList = []
+        for i in xrange(len(segregatedList[index])):
+            point = om.MPoint()
+            edgeMesh.getPoint(segregatedList[index][i], point)
+            pointList.append(point)
+            print "point # " + str(segregatedList[index][i]) + " appended to pointList " + str(index) + " with coordinates X: " + str(point[0]) + " Y: " + str(point[1]) + " Z: " + str(point[2])  
+        masterList.append(pointList)
+        print len(masterList)
+    
+    return masterList   # list of lists of MPoints segregated into their own divisions
 
 # print matrix contents for debugging
 def printMatrix(matrix):
@@ -211,9 +231,8 @@ def deleteNoodles():
     if len(noodleList) > 0:               
         cmds.delete (noodleList)      
 
-# create base noodle (cylinder) with arguments numSections (each section with a unit length of 1) and rad (cylinder radius)
+# create base noodle (cylinder) with arguments numSections (each section with a unit length of 1), rad (cylinder radius)
 def createNoodle(numSections, rad):
-    """Creates the base cylinder. Accepts numSections.  Set height of each section as 1, thus making height = y-subdivisions = numSections."""
     result = cmds.polyCylinder( radius = rad, axis = [1, 0, 0], height = numSections , sy = numSections, name = 'pNoodle#' )
-    print 'result: ' + str( result )
+    print 'result: ' + str(result)
     return result
